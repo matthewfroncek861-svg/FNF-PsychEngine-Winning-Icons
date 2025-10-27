763485c1dd65a46aed8e5b4d6a6d8bc29ffb1902
matthewfroncek861-svg <matthewfroncek861@gmail.com>
2025-10-27 13:17:02 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,7 +4,7 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
+haxelib git lime https://github.com/openfl/lime 1409f6c00d5517a14af66dfdda420168173eeb9c
 haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
